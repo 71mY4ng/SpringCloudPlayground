8a9ad2d04c61c98452fa920bfd7b9964177b969f
TimYang <timyang2016@foxmail.com>
2020-10-24 16:08:34 +0800

    add update and create check greet

--- play-transaction/src/main/java/com/timyang/playground/transaction/controller/GreetController.java
+++ play-transaction/src/main/java/com/timyang/playground/transaction/controller/GreetController.java
@@ -3,16 +3,22 @@ package com.timyang.playground.transaction.controller;
 import com.timyang.playground.transaction.dao.TokenRecordRepository;
 import com.timyang.playground.transaction.entitys.TokenRecord;
 import com.timyang.playground.transaction.util.IdGenService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
-@RestController("greet")
+@RestController
+@RequestMapping("/greet")
 public class GreetController {
+
+    private static final Logger log = LoggerFactory.getLogger(GreetController.class);
 
     private final IdGenService idGenService;
 
@@ -25,8 +31,21 @@ public class GreetController {
         this.redisTokenRecordRepo = redisTokenRecordRepository;
     }
 
-    @RequestMapping
+    @GetMapping(value = "/greeting")
     public String greeting(@RequestParam String name) {
+
+        log.debug("greeting transaction comes from {}", name);
+
+        Optional<TokenRecord> record = Optional.ofNullable(redisTokenRecordRepo.getTokenRecordByUsername(name));
+
+        if (record.isPresent()) {
+            TokenRecord tokenRecord = record.get();
+
+            return "Hello! Dear " + name + ", you already have token: "
+                    + tokenRecord.getToken() +
+                    " at " + tokenRecord.getGenTime().format(DateTimeFormatter.ISO_DATE_TIME);
+        }
+
         LocalDateTime createTime = LocalDateTime.now();
         String token = idGenService.getId(createTime);
 
@@ -34,5 +53,26 @@ public class GreetController {
 
         return "Hello! Dear " + name + ", you get token: "
                 + token + " at " + createTime.format(DateTimeFormatter.ISO_DATE_TIME);
+    }
+
+    @PostMapping(value = "/update")
+    public TokenRecord updateToken(@RequestParam String name) {
+        log.debug("{} request for an update of the token", name);
+
+        LocalDateTime createTime = LocalDateTime.now();
+        String token = idGenService.getId(createTime);
+
+        final TokenRecord neoToken = new TokenRecord(name, token, createTime);
+        redisTokenRecordRepo.updateTokenRecordByUsername(name, neoToken);
+
+        return neoToken;
+    }
+
+    @GetMapping(value = "/list")
+    public List<TokenRecord> list() {
+
+        log.debug("list tokenRecord");
+
+        return Collections.emptyList();
     }
 }
