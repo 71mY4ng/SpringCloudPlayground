23e1a63600802e5c81fc66b84c595c420d1a40a1
timyang <timyang2016@foxmail.com>
2021-01-15 00:52:55 +0800

    component exclude rule

--- play-filebrowse/src/main/java/com/timyang/playground/filebrowse/FileBrowseApplication.java
+++ play-filebrowse/src/main/java/com/timyang/playground/filebrowse/FileBrowseApplication.java
@@ -2,7 +2,12 @@ package com.timyang.playground.filebrowse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
+//@ComponentScan(excludeFilters = {
+//        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = {RestTestController.class, BrowseController.class})
+//})
 @SpringBootApplication
 public class FileBrowseApplication {
 
