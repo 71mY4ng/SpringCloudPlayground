ead42ec3549a85fc08417ce273f11ef7a6e13a9c
timyang <timyang2016@foxmail.com>
2021-01-17 21:03:11 +0800

    download file controller

--- play-filebrowse/src/main/java/com/timyang/playground/filebrowse/RestTestController.java
+++ play-filebrowse/src/main/java/com/timyang/playground/filebrowse/RestTestController.java
@@ -1,13 +1,15 @@
 package com.timyang.playground.filebrowse;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Slf4j
 @RestController
@@ -25,20 +27,35 @@ public class RestTestController {
                 .body("hello");
     }
 
-//    @GetMapping(value = "/greeting/1/{bizDate}/{entityNo}")
-//    ResponseEntity<StreamingResponseBody> greeting(
-//            @PathVariable String bizDate,
-//            @PathVariable String entityNo,
-//            final HttpServletResponse response
-//    ) {
-//        response.setContentType("text/csv");
+    @GetMapping(value = "/download")
+    ResponseEntity<StreamingResponseBody> download(
+            final HttpServletResponse response
+    ) throws IOException {
+        response.setContentType("text/csv");
 //        response.setHeader(
 //                "Content-Disposition",
 //                "attachment;filename=sample.csv");
-//
-//        StreamingResponseBody stream = out -> {
-//
-//        };
-//
-//    }
+
+        final InputStream is = Files.newInputStream(Paths.get("/home/timyang/Music/test4000file/file3821.bin"));
+
+        StreamingResponseBody stream = os -> {
+
+            byte[] data = new byte[2048];
+            int read = 0;
+            while ((read = is.read(data)) >= 0) {
+                os.write(data, 0, read);
+            }
+            os.flush();
+        };
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setConnection("close");
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .cacheControl(CacheControl.noCache())
+                .headers(headers)
+                .body(stream);
+    }
+
 }
